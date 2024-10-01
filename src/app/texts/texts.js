8d5032c2a90e777d94fc1b5c870474750da1f83a
kevinpicon01 <digitalpruu@gmail.com>
2024-10-01 17:27:59 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,36 +1,36 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Costurero la Aguja de Oro";
+const slogan = "Dale una segunda vida a tus prendas";
+const email = "anaeyda.ruiz@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los servicios de ${title} y me gustaría recibir más información. ¡Espero su respuesta!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3208989588";
 const textos = {
     index: {
         title: title,
+        slogan: slogan,
     },
     homeContent: {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "*Costurero la Aguja de Oro* es tu mejor aliado en el cuidado y arreglo de prendas. Con años de experiencia en el arte de la costura, garantizamos que cada pieza sea reparada con precisión y esmero.",
+        description2: "Nuestra misión es devolverle vida a tus prendas favoritas, asegurándonos de que queden como nuevas. En *Costurero la Aguja de Oro*, cada detalle cuenta para ofrecerte resultados de calidad.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Costurero la Aguja de Oro",
+            p2: slogan,
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/profile.php?id=61565527634366&mibextid=ZbWKwL",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
 export default textos;
